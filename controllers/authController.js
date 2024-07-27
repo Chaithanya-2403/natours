abd812239f7d7f1e9b1bca79765979debf0db433
Chaithanya <chaithanya@haspit.com>
2024-07-27 11:59:13 +0530

    Added readme file and modified authController file

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken');
 const email = require('../utils/email');
 const Email = require('../utils/email');
 const mongoose = require('mongoose');
+
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 };
